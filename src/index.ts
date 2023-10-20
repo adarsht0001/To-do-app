9e40183e51afff50a452fbbfddf018284d669eca
adarsht0001 <adarsht00001@gmail.com>
2023-10-21 01:31:26 +0530

    add: setup prisma

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ const app = new Elysia();
 app.get("/", () => "Hello Elysia");
 
 app.post("/", ({ body, set }) => {
-  console.log(body);
-  return "invalid";
+    console.log(body);
+    return "invalid";
 });
 
 app.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
