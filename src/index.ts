c24c959cce60776bede6acd1d9e5baef5b77ace7
adarsht0001 <adarsht00001@gmail.com>
2023-11-02 22:37:35 +0530

    add: prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Elysia();
+
+const prisma = new PrismaClient();
 
 app.get("/", () => "Hello Elysia");
 
